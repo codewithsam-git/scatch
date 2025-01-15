4e83d2daccdba8c87728e93b1a8ecad8c9bc2aa1
codewithsam-git <samarthb.2525@gmail.com>
2025-01-15 22:11:19 +0530

    models

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser()) // use to read cookies on another routes
+
+
 app.get('/', function(req, res){
     res.send("hello");
 })
