d5e0fa419d47f0866ae2e2c8261d5b5c1b7e4b63
codewithsam-git <samarthb.2525@gmail.com>
2025-01-18 07:36:41 +0530

    controller & common token generator created

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const ownersRouter = require('./routes/ownersRouter');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
 const app = express();
-
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
+require('dotenv').config();  // helps to use value from .env to another files
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,35 +1,13 @@
 const express = require('express');
 const router = express.Router()
-const userModel = require('../models/user-model');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { registerUser } = require('../controllers/authController');
+const { loginUser } = require('../controllers/authController');
 
 router.get('/', function (req, res) {
     res.send("usersRouter working");
 });
 
-router.post('/register', function (req, res) {
-    try {
-        let { fullname, email, password } = req.body;
-        bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(password, salt, async function (err, hash) {
-                if (err) return res.send(err.message)
-                else {
-                    let user = await userModel.create({
-                        fullname,
-                        email,
-                        password: hash
-                    });
-                    let token = jwt.sign({ email, userId: user._id }, "secret")
-                    res.cookie("token", token);
-                    // console.log(req.cookies.token);
-                    res.send("User created successfully.");
-                }
-            })
-        })
-    } catch (err) {
-        res.send("Register Route: ", err.message);
-    }
-})
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router
